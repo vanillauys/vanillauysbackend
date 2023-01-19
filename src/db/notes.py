c2386a5f3d9b7b132c16d866c05d9f01457f300b
Wihan Uys <wihan.uys@gmail.com>
2023-01-19 18:05:00 +0200

    v0.1.8 Users can now make notes.

--- src/db/notes.py
+++ src/db/notes.py
@@ -21,6 +21,7 @@ def create_note(email: str, title: str, body: str):
         return False, f'the note {title} already exists in the db.'
 
     note = {
+        'email': email,
         'title': title,
         'body': body
     }
@@ -39,3 +40,44 @@ def check_notes(email: str, title: str):
         return True, note
     except Exception:
         return False, None
+
+
+def delete_note(key: str):
+    try:
+        notes.delete(key)
+        return True, f'succesfully deleted {key} from db.'
+    except Exception:
+        return False, f'an error occured while trying to delete {key} from db.'
+
+
+def update_note(key: str, title: str, body: str):
+    updates = {
+        'title': title,
+        'body': body
+    }
+    try:
+        notes.update(updates, key)
+        return True, 'successfully updated the note in db.'
+    except Exception:
+        return False, 'an error occurred while updating the note.'
+
+
+def get_all_notes(email: str):
+    try:
+        results = notes.fetch({'email': email})
+        note = results.items
+        return True, note
+    except Exception:
+        return False, f'an error occurred while fetching notes for {email}.'
+
+
+def get_note(key: str):
+    try:
+        result = notes.get(key)
+        if result is None:
+            return False, f'no note with key {key} found in db.'
+        
+        del result['email']
+        return True, result
+    except:
+        return False, f'an error occurred while getting note with key {key}'
