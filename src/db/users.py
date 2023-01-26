d1c1e69110d213e51132cd576d3283964c7411a3
Wihan Uys <wihan.uys@gmail.com>
2023-01-26 14:21:28 +0200

    v0.3.1. Users now use classes.

--- src/db/users.py
+++ src/db/users.py
@@ -3,69 +3,91 @@
 # ---------------------------------------------------------------------------- #
 
 
-from db.db_conf import users
-from schemas import UserSchema
-from schemas import UserLoginSchema
+import os
+from deta import Deta
+from dotenv import load_dotenv
+from schemas import Schemas
 from auth.auth_manager import Auth
+from typing import Dict, Tuple
 
 
 # ---------------------------------------------------------------------------- #
-# --- Main ------------------------------------------------------------------- #
+# --- User Database ---------------------------------------------------------- #
 # ---------------------------------------------------------------------------- #
 
-ah = Auth()
+
+load_dotenv()
+
+class UserDB():
+
+    auth = Auth()
+    schemas = Schemas()
+    PROJECT_KEY = os.getenv('DETA_PROJECT_KEY')
+    deta = Deta(PROJECT_KEY)
+    users = deta.Base('users')
+
+    def create_user(self, user: schemas.user_schema()) -> Tuple[int, str]:
+        code, _, _ = self.get_user_by_email(user.email)
+        if code == 200:
+            return 409, f"email '{user.email}' already exists in the db."
+        if code == 500:
+            return 500, f"an error occured while checking if email '{user.email}' exists in the db."
+
+        code, _, _ = self.get_user_by_username(user.username)
+        if code == 200:
+            return 409, f"username '{user.username}' already exists in the db."
+        if code == 500:
+            return 500, f"an error occured while checking if username '{user.username}' exists in the db."
 
 
-def create_user(user: UserSchema):
-    status, email = check_email(user.email)
-    if not status:
-        return False, 'an error occured when trying to check if the email address is in use.'
-    if email:
-        return False, f'email {user.email} already exists in the db.'
+        data = {
+            'username': user.username,
+            'email': user.email,
+            'password': self.auth.hash_password(user.password)
+        }
 
-    status, username = check_username(user.username)
-    if not status:
-        return False, 'an error occured when trying to check if the username is in use.'
-    if username:
-        return False, f'username {user.username} already exists in the db.'
-
-    data = {
-        'username': user.username,
-        'email': user.email,
-        'password': ah.hash_password(user.password)
-    }
-    result = users.put(data)
-    return True, result
+        try:
+            self.users.put(data)
+            return 200, f"successfully added user '{user.email}' to the db."
+        except Exception:
+            return 500, 'an error occured while adding user to the db.'            
 
 
-def login_user(user: UserLoginSchema):
-    status, data = check_email(user.email)
+    def login_user(self, user: schemas.user_login_schema()) -> Tuple[int, str]:
+        code, response, result = self.get_user_by_email(user.email)
 
-    if not status:
-        return False, f'an error occured while trying to log in {user.email}.'
+        if code != 200:
+            return code, response
 
-    if not data:
-        return False, f'{user.email} does not exist in db.'
+        if not self.auth.verify_password(user.password, result['password']):
+            return 401, f"invalid password for '{user.email}'."
 
-    if not ah.verify_password(user.password, data[0]['password']):
-        return False, f'invalid password for {user.email}.'
-
-    return True, f'{user.email} logged in successfully.'
+        return 200, f"'{user.email}' logged in successfully."
 
 
-def check_email(email: str):
-    try:
-        results = users.fetch({'email': email})
-        user = results.items
-        return True, user
-    except Exception:
-        return False, None
+    def get_user_by_email(self, email: str) -> Tuple[int, str, Dict]:
+        try:
+            results = self.users.fetch({'email': email})
+            user = results.items
+
+            if not user:
+                return 404, f"email '{email}' not found in db.", None
+
+            return 200, f"email '{email}' found in db.", user[0]
+
+        except Exception:
+            return 500, f"an error occurred while fetching '{email}'", None
 
 
-def check_username(username: str):
-    try:
-        results = users.fetch({'username': username})
-        user = results.items
-        return True, user
-    except Exception:
-        return False, None
+    def get_user_by_username(self, username: str) -> Tuple[int, str, Dict]:
+        try:
+            results = self.users.fetch({'username': username})
+            user = results.items
+
+            if not user:
+                return 404, f"username '{username}' not found in db.", None
+            
+            return 200, f"username '{username}' found in db.", user[0]
+
+        except Exception:
+            return 500, f"an error occurred while fetching '{username}'", None
--- src/routers/users.py
+++ src/routers/users.py
@@ -6,8 +6,8 @@
 from fastapi import APIRouter, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi.responses import JSONResponse
-from schemas import Message, UserLoginSchema, UserSchema, LoggedIn
-from db.users import create_user, login_user
+from schemas import Schemas
+from db.users import UserDB
 from auth.auth_manager import Auth
 
 
@@ -18,7 +18,9 @@ from auth.auth_manager import Auth
 
 router = APIRouter()
 security = HTTPBearer()
-auth_handler = Auth()
+auth = Auth()
+schemas = Schemas()
+db = UserDB()
 
 
 # ---------------------------------------------------------------------------- #
@@ -27,41 +29,33 @@ auth_handler = Auth()
 
 
 @router.post('/users/signup', tags=['Users'],
-             response_model=Message,
-             responses={
-    500: {"model": Message},
-    409: {"model": Message}
-})
-def sign_up(user: UserSchema):
-    status, result = create_user(user)
-    if status:
-        response = {
-            'message': f'successfully added {user.email} to db.'
-        }
-        return JSONResponse(status_code=200, content=response)
-    else:
-        response = {
-            'message': result
-        }
-        return JSONResponse(status_code=409, content=response)
+    response_model=schemas.detail(),
+    responses={
+        409: {"model": schemas.detail()},
+        500: {"model": schemas.detail()}
+    }
+)
+def sign_up(user: schemas.user_schema()):
+    code, response = db.create_user(user)
+    return JSONResponse(status_code=code, content={'detail': response})
 
 
 @router.post('/users/login', tags=['Users'],
-             response_model=LoggedIn,
-             responses={
-    500: {"model": Message},
-    401: {"model": Message}
-})
-def login(user: UserLoginSchema):
-    status, result = login_user(user)
-    response = {
-        'message': result
+    response_model=schemas.logged_in(),
+    responses={
+        401: {"model": schemas.detail()},
+        404: {"model": schemas.detail()},
+        500: {"model": schemas.detail()}
     }
-    if not status:
-        return JSONResponse(status_code=401, content=response)
+)
+def login(user: schemas.user_login_schema()):
+    code, response = db.login_user(user)
 
-    access_token = auth_handler.encode_token(user.email)
-    refresh_token = auth_handler.encode_refresh_token(user.email)
+    if code != 200:
+        return JSONResponse(status_code=code, content={'detail': response})
+
+    access_token = auth.encode_token(user.email)
+    refresh_token = auth.encode_refresh_token(user.email)
     response = {
         'email': user.email,
         'access_token': access_token,
@@ -71,24 +65,26 @@ def login(user: UserLoginSchema):
 
 
 @router.get('/users/potected', tags=['Users'],
-            response_model=Message,
-            responses={
-    401: {"model": Message},
-    500: {"model": Message}
-})
+    response_model=schemas.detail(),
+    responses={
+        401: {"model": schemas.detail()},
+        500: {"model": schemas.detail()}
+    }
+)
 def test_protected(credentials: HTTPAuthorizationCredentials = Security(security)):
     token = credentials.credentials
-    if auth_handler.decode_token(token):
-        return JSONResponse(status_code=200, content={'message': 'you are logged in.'})
+    if auth.decode_token(token):
+        return JSONResponse(status_code=200, content={'detail': 'you are logged in.'})
 
 
 @router.get('/users/refresh_token', tags=['Users'],
-            response_model=Message,
-            responses={
-    401: {"model": Message},
-    500: {"model": Message}
-})
+    response_model=schemas.detail(),
+    responses={
+        401: {"model": schemas.detail()},
+        500: {"model": schemas.detail()}
+    }
+)
 def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security)):
     refresh_token = credentials.credentials
-    new_token = auth_handler.refresh_token(refresh_token)
-    return JSONResponse(status_code=200, content={'message': new_token})
+    new_token = auth.refresh_token(refresh_token)
+    return JSONResponse(status_code=200, content={'detail': new_token})
