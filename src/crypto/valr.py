6e7881db91498e4bbf94c34d2b025704d5703f56
Wihan Uys <wihan.uys@gmail.com>
2023-03-04 18:42:49 +0200

    v0.6.0. Async request crypto prices. foo

--- src/crypto/valr.py
+++ src/crypto/valr.py
@@ -6,7 +6,6 @@
 import httpx
 import asyncio
 from typing import Dict, Tuple
-from time import perf_counter
 
 
 # ---------------------------------------------------------------------------- #
@@ -50,7 +49,6 @@ class Valr():
 
 
     async def get_exchange_rates(self) -> Tuple[int, str, list[Dict[str, float]]]:
-        start = perf_counter()
         values = [0] * 8
         urls = self.get_url_list()
         headers = {}
@@ -61,8 +59,6 @@ class Valr():
         try:
             for index, value in enumerate(results):
                 values[index] = value.json()
-            end = perf_counter()
-            print(f"Took {end - start} seconds.")
             return 200, 'successfully received exchange rate from valr.', self.formatted(values)
         except Exception:
             return 500, 'error receiving exchange rate from valr.', None
