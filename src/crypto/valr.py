918163e35b3939e6b74da491f920dec5da62af23
Wihan Uys <wihan.uys@gmail.com>
2023-03-04 18:35:21 +0200

    Async testing

--- src/crypto/valr.py
+++ src/crypto/valr.py
@@ -4,6 +4,7 @@
 
 
 import httpx
+import asyncio
 from typing import Dict, Tuple
 
 
@@ -14,78 +15,51 @@ from typing import Dict, Tuple
 
 class Valr():
 
+    cryptos = {
+        'BTCZAR': 'Bitcoin',
+        'ETHZAR': 'Ethereum',
+        'XRPZAR': 'Ripple XRP',
+        'SOLZAR': 'Solana',
+        'BNBZAR': 'Binance Coin',
+        'USDCZAR': 'USD Coin',
+        'SHIBZAR': 'Shib Token',
+        'AVAXZAR': 'Avalanche'
+    }
+
     def url(self, pair: str) -> str:
         return f"https://api.valr.com/v1/public/{pair}/marketsummary"
 
 
+    def get_url_list(self) -> list:
+        urls = [0] * 8
+        for index, (key, _) in enumerate(self.cryptos.items()):
+            urls[index] = self.url(key)
+        return urls
+
+
     def formatted(self, values: list) -> list[Dict[str, float]]:
-        results = []
-        results.append({
-            'name': 'Bitcoin',
-            'price': values[0]['lastTradedPrice'],
-            'change': values[0]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'Ethereum',
-            'price': values[1]['lastTradedPrice'],
-            'change': values[1]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'Ripple XRP',
-            'price': values[2]['lastTradedPrice'],
-            'change': values[2]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'Solana',
-            'price': values[3]['lastTradedPrice'],
-            'change': values[3]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'Binance Coin',
-            'price': values[4]['lastTradedPrice'],
-            'change': values[4]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'USD Coin',
-            'price': values[5]['lastTradedPrice'],
-            'change': values[5]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'Shib Token',
-            'price': values[6]['lastTradedPrice'],
-            'change': values[6]['changeFromPrevious']
-        })
-        results.append({
-            'name': 'Avalanche',
-            'price': values[7]['lastTradedPrice'],
-            'change': values[7]['changeFromPrevious']
-        })
+        results = [0] * 8
+        for index, value in enumerate(values):
+            results[index] = {
+                'name': self.cryptos[value['currencyPair']],
+                'price': value['lastTradedPrice'],
+                'change': value['changeFromPrevious']
+            }
         return results
 
 
     async def get_exchange_rates(self) -> Tuple[int, str, list[Dict[str, float]]]:
+        values = [0] * 8
+        urls = self.get_url_list()
+        headers = {}
         async with httpx.AsyncClient() as client:
-            btc = await client.get(self.url('BTCZAR'), headers={})
-            eth = await client.get(self.url('ETHZAR'), headers={})
-            xrp = await client.get(self.url('XRPZAR'), headers={})
-            sol = await client.get(self.url('SOLZAR'), headers={})
-            bnb = await client.get(self.url('BNBZAR'), headers={})
-            usdc = await client.get(self.url('USDCZAR'), headers={})
-            shib = await client.get(self.url('SHIBZAR'), headers={})
-            avax = await client.get(self.url('AVAXZAR'), headers={})
+            reqs = [client.get(url, headers=headers) for url in urls]
+            results = await asyncio.gather(*reqs)
 
         try:
-            values = [
-                btc.json(),
-                eth.json(),
-                xrp.json(),
-                sol.json(),
-                bnb.json(),
-                usdc.json(),
-                shib.json(),
-                avax.json()
-            ]
-
+            for index, value in enumerate(results):
+                values[index] = value.json()
+            print(f"VALUES IS: {values}")
             return 200, 'successfully received exchange rate from valr.', self.formatted(values)
         except Exception:
             return 500, 'error receiving exchange rate from valr.', None
